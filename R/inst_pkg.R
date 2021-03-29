4b23e190f2fde20f31f12f09a6dae7855120bdd3
Silvan Albin <silvan.albin@neon-free.ch>
2021-03-29 23:35:09 +0200

    Adding existing files to package

---
+++ R/inst_pkg.R
@@ -0,0 +1,16 @@
+#' Install and load packages
+#'
+#' This function allows you to install and load all your packages. Additionally
+#' it is shown whether the package was loaded successfully or not.
+#' @param pkg A package for loading your packages - simple as that
+#' @keywords pkg
+#' @export inst_pkg
+#' @examples
+#' inst_pack()
+
+inst_pkg <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
