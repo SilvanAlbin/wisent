35fffbe7164538e9eecf196bae002ce34810f3ab
Silvan Albin <silvan.albin@neon-free.ch>
2021-04-02 10:48:03 +0200

    Commiting changes for function date_format()

--- R/date_format.R
+++ R/date_format.R
@@ -1,17 +1,21 @@
-#' Install and load packages
+#' Transforming Date column
 #'
-#' This function allows you to install and load all your packages. Additionally
-#' it is shown whether the package was loaded successfully or not.
-#' @param df,date Change the date format
+#' Following function transforms the Date as class Date and creates a new
+#' columns with the Date as Month and Year. This is especially handy for
+#' the creation of visualization as it takes less space and avoids overlapping
+#' text on the axises.
+#'
+#' @param df The data table
+#' @param date The column indicating the date
 #' @export date_format
 #' @examples
-#' df = some data
-#' inst_pkg(df, date)
+#' df <- data.frame (names  = c('John Doe','Peter Gynn','Jolie Hope'),
+#' date = c('2010-11-1','2008-3-25','2007-3-14'))
+#' date_format(df, date)
 
 date_format <- function(df, date) {
-  
+  df$date = as.Date(df$date)
+  df$Date = format(as.Date(df$date), format = "%b %y")
 }
 
-df2$Date <- format(as.Date(df2$date), format = "%b %y")
-
-df2$date = as.Date(df2$date)
+df = date_format(df,date)
