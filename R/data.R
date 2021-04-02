c6da56447f50887c695327acf75c169632caea82
Silvan Albin <silvan.albin@neon-free.ch>
2021-04-02 11:28:54 +0200

    Adding data for the package

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Prices of over 50,000 round cut diamonds
+#'
+#' A dataset containing random data. The variables are as follows:
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{name}{name}
+#'   \item{date}{random date}
+#'   \item{payment}{amount of payment}
+#' }
+"df"
--- R/date_format.R
+++ R/date_format.R
@@ -3,14 +3,12 @@
 #' Following function transforms the Date as class Date and creates a new
 #' columns with the Date as Month and Year. This is especially handy for
 #' the creation of visualization as it takes less space and avoids overlapping
-#' text on the axises.
+#' text on the axes.
 #'
 #' @param df The data table
 #' @param date The column indicating the date
 #' @export date_format
 #' @examples
-#' df <- data.frame (names  = c('John Doe','Peter Gynn','Jolie Hope'),
-#' date = c('2010-11-1','2008-3-25','2007-3-14'))
 #' date_format(df, date)
 
 date_format <- function(df, date) {
@@ -18,4 +16,5 @@ date_format <- function(df, date) {
   df$Date = format(as.Date(df$date), format = "%b %y")
 }
 
-df = date_format(df,date)
+df$Date = date_format(df,date)
+
